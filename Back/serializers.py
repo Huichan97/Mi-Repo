fd1999a7e90bb39822d7ccb14da084ad60f346ce
Felipe <fe.huichan@duocuc.cl>
2024-10-11 20:41:18 -0300

    si
    si

--- Back/serializers.py
+++
@@ -1,1 +0,0 @@
-from django.shorcuts import path
--- back/src/testigos/testigos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestigosController } from './testigos.controller';
-import { TestigosService } from './testigos.service';
-
-@Module({
-  controllers: [TestigosController],
-  providers: [TestigosService],
-})
-export class TestigosModule {}
